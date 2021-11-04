8f66b1bc146b42f06bca969443252a26662e03a3
jifishma <44071786+jifishma@users.noreply.github.com>
2021-11-04 12:30:14 -0400

    * Update test plans * Working win logic * More responsive design :)

--- Project 02/Game/scripts/game.js
+++ Project 02/Game/scripts/game.js
@@ -16,13 +16,13 @@ var gameData = {
     stalemate: true
 }
 
-window.onload = function() {
+window.onload = function () {
     gameData.cells = document.querySelectorAll(".cell");
     gameData.board = document.querySelector(".board");
     gameData.turnStatus = document.querySelector(".turnStatus");
     gameData.playerPieceStatus = document.querySelector(".playerPieceStatus");
     gameData.startButton = document.querySelector("#startGame");
-    
+
     resetBoard();
 }
 //
@@ -39,6 +39,8 @@ function startGame() {
     gameData.playing = true;
     gameData.startButton.disabled = true;
 
+    gameData.board.classList.add("playable");
+
     if (gameData.opponentIsCPU && !gameData.playerTurn)
         cpuPlayPiece();
 }
@@ -46,6 +48,7 @@ function startGame() {
 function resetBoard() {
     gameData.cells.forEach(cell => {
         cell.textContent = "";
+        cell.classList.remove("winLine");
     });
 
     gameData.turnStatus.textContent = "No game in progress.";
@@ -57,14 +60,15 @@ function resetBoard() {
     gameData.opponentIsCPU = false;
 
     gameData.startButton.disabled = false;
+    gameData.board.classList.remove("playable");
 }
 //
 
-// player interaction
-function placePiece(row, col)  {
-    let index = row + (col * 3)
+// player board interaction
+function placePiece(col, row) {
+    let index = col + (row * 3)
 
-    if ((gameData.opponentIsCPU && !gameData.playerTurn) || !gameData.playing || cellIsOccupied(index)) 
+    if ((gameData.opponentIsCPU && !gameData.playerTurn) || !gameData.playing || cellIsOccupied(index))
         return;
 
     occupyCell(index);
@@ -79,8 +83,8 @@ function placePiece(row, col)  {
 //
 
 // light logic
-function sleep(timeMs) {
-    return new Promise(resolve => setTimeout(resolve, timeMs));
+function sleep(milliSeconds) {
+    return new Promise(resolve => setTimeout(resolve, milliSeconds));
 }
 
 function selectStartingPlayer() {
@@ -99,11 +103,15 @@ function cellIsOccupied(index) {
     return gameData.cells[index].textContent != ""
 }
 
+function getCellPiece(index) {
+    return gameData.cells[index].textContent;
+}
+
 function updateStatus() {
     let status = "";
     if (gameData.playerTurn)
         status = "player's"
-    else 
+    else
         status = gameData.opponentIsCPU ? "CPU's" : "opponent's";
 
     gameData.turnStatus.textContent = "It's the " + status + " turn.";
@@ -116,6 +124,7 @@ function cpuPlayPiece() {
 
 function endGame() {
     gameData.playing = false;
+    gameData.board.classList.remove("playable");
 
     gameData.playerPieceStatus.textContent = "Game over, ";
 
@@ -128,18 +137,22 @@ function endGame() {
 
     gameData.playerPieceStatus.textContent += "won!"
 }
+
+function mod(num, mod) {
+    return ((num % mod) + mod) % mod;
+}
 //
 
 // heavy logic
 function cpuOccupyCell() {
     let index = 0;
     let panic = 1000;
-    
+
     do {
         index = Math.floor(Math.random() * gameData.cells.length);
         panic--;
     } while (cellIsOccupied(index) || !panic);
-    
+
     if (!panic) {
         alert("Panic!");
         return;
@@ -149,32 +162,95 @@ function cpuOccupyCell() {
     updateStatus();
 }
 
-function occupyCell(cellIndex) {
-    let cell = gameData.cells[cellIndex]
+function occupyCell(index) {
+    let cell = gameData.cells[index]
 
     let piece = gameData.playerTurn ? getPlayerPiece() : getOpponentPiece();
     cell.textContent = piece;
     gameData.placedPieces++;
 
-
-    checkBoard();
-
-    if (gameData.placedPieces >= gameData.cells.length) {
-        endGame();
+    if (checkBoard(index))
         return;
-    }
 
     gameData.playerTurn = !gameData.playerTurn;
 }
 
-function checkBoard() {
-    let pieceToCheck = gameData.playerTurn ? getPlayerPiece() : getOpponentPiece();
-    
-    for (let row = 0; row < 3; row++) {
-        for (let col = 0; col < 3; col++) {
-            let index = row + (col * 3);
-            console.log(gameData.cells[index] === pieceToCheck);
-        }
+function checkBoard(originIndex) {
+    console.log("----------------------")
+    console.log(originIndex);
+
+    if (processLine(originIndex, 1, 0)) {
+        endGame();
+        return true;
     }
+
+    if (processLine(originIndex, 0, 1)) {
+        endGame();
+        return true;
+    }
+
+    if (processLine(0, 1, 1, true)) {
+        endGame();
+        return true;
+    }
+
+    if (processLine(6, 1, -1, true)) {
+        endGame();
+        return true;
+    }
+
+    console.log("----------------------\n")
+
+    if (gameData.placedPieces >= gameData.cells.length) {
+        endGame();
+        return true;
+    }
+
+    return false;
+}
+
+function processLine(originIndex, rowIncrement, colIncrement, debug) {
+    let pieceToCheck = gameData.playerTurn ? getPlayerPiece() : getOpponentPiece();
+
+    let cRow = Math.floor(originIndex / 3);
+    let cCol = mod(originIndex, 3);
+    let index = originIndex;
+
+    let matches = [];
+    for (let i = 0; i < 3; i++) {
+        index = cCol + (cRow * 3)
+
+        if (debug) {
+            console.log(cRow, cCol);
+            console.log(getCellPiece(index))
+        }
+
+        if (getCellPiece(index) != pieceToCheck)
+            break;
+
+        matches.push(gameData.cells[index]);
+
+        cRow += rowIncrement;
+        cRow = mod(cRow, 3);
+
+        cCol += colIncrement;
+        cCol = mod(cCol, 3);
+    }
+
+    if (matches.length >= 3) {
+        gameData.playing = false;
+        gameData.stalemate = false;
+
+        if (gameData.playerTurn)
+            gameData.playerWon = true;
+
+        matches.forEach(match => {
+            match.classList.add("winLine");
+        });
+
+        return true;
+    }
+
+    return false;
 }
 //
